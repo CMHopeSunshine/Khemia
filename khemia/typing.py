b24f3470551fab3ceff487c0fd56f8794163fcfd
CMHopeSunshine <277073121@qq.com>
2024-05-08 14:58:43 +0800

    :bookmark: bump version 0.1.1

--- khemia/typing.py
+++ khemia/typing.py
@@ -1,16 +1,25 @@
+from collections.abc import Iterable, Mapping, Sequence
 import contextlib
 from dataclasses import is_dataclass as is_dataclass
 import sys
 import typing
-from typing import Callable, ClassVar, ForwardRef, Iterable, Mapping, Optional, Sequence, Tuple, Type, Union
-from typing_extensions import (
+from typing import (
     Annotated,
+    Callable,
+    ClassVar,
+    ForwardRef,
+    Optional,
+    TYPE_CHECKING,
+    Union,
+)
+from typing_extensions import (
     Any,
     get_args as _typing_get_args,
     get_origin as _typing_get_origin,
     is_protocol as is_protocol,
     is_typeddict as is_typeddict,
     Literal,
+    TypeGuard,
 )
 
 LITERAL_TYPES: set[Any] = {Literal}
@@ -20,33 +29,40 @@ if hasattr(typing, "Literal"):
 NoneType = type(None)
 NONE_TYPES: tuple[Any, ...] = (None, NoneType, *(tp[None] for tp in LITERAL_TYPES))
 
+if TYPE_CHECKING:
+    check_isinstance = isinstance
+else:
 
-def check_isinstance(value: Any, class_or_tuple: type[Any] | tuple[type[Any], ...] | None) -> bool:
-    try:
-        return isinstance(value, class_or_tuple)  # type: ignore[arg-type]
-    except TypeError:
-        return False
-
-
-def check_issubclass(cls: Any, class_or_tuple: Any) -> bool:
-    try:
-        return isinstance(cls, type) and issubclass(cls, class_or_tuple)
-    except TypeError:
-        return False
+    def check_isinstance(value: Any, class_or_tuple: Union[type[Any], tuple[type[Any], ...], None]) -> bool:
+        try:
+            return isinstance(value, class_or_tuple)
+        except TypeError:
+            return False
 
 
-def get_origin(tp: Type[Any]):
+if TYPE_CHECKING:
+    check_issubclass = issubclass
+else:
+
+    def check_issubclass(cls: Any, class_or_tuple: Any) -> bool:
+        try:
+            return isinstance(cls, type) and issubclass(cls, class_or_tuple)
+        except TypeError:
+            return False
+
+
+def get_origin(tp: type[Any]):
     return _typing_get_origin(tp) or getattr(tp, "__origin__", None)
 
 
-def _generic_get_args(tp: Type[Any]) -> Tuple[Any, ...]:
+def _generic_get_args(tp: type[Any]) -> tuple[Any, ...]:
     with contextlib.suppress(TypeError):
-        if tp == Tuple[()] or sys.version_info >= (3, 9) and tp == tuple[()]:
+        if tp == tuple[()] or sys.version_info >= (3, 9) and tp == tuple[()]:
             return ((),)
     return ()
 
 
-def get_args(tp: Type[Any]) -> Tuple[Any, ...]:
+def get_args(tp: type[Any]) -> tuple[Any, ...]:
     return _typing_get_args(tp) or getattr(tp, "__args__", ()) or _generic_get_args(tp)
 
 
@@ -82,7 +98,7 @@ def is_namedtuple(type_: type[Any]) -> bool:
     return check_issubclass(type_, tuple) and hasattr(type_, "_fields")
 
 
-def _check_classvar(v: type[Any] | None) -> bool:
+def _check_classvar(v: Union[type[Any], None]) -> bool:
     if v is None:
         return False
 
@@ -100,50 +116,50 @@ def is_classvar(type_: type[Any]) -> bool:
 
 if sys.version_info < (3, 10):
 
-    def is_union(type_: Optional[Type[Any]]) -> bool:
+    def is_union(type_: Optional[type[Any]]) -> bool:
         return type_ is Union
 
 
 else:
     import types
 
-    def is_union(type_: Optional[Type[Any]]) -> bool:
+    def is_union(type_: Optional[type[Any]]) -> bool:
         return type_ is Union or type_ is types.UnionType
 
 
-def is_optional(type: Type[Any]) -> bool:
-    return is_union(get_origin(type)) and any(is_none_type(tp) for tp in get_args(type))
+def is_optional(type_: type[Any]) -> bool:
+    return is_none_type(type_) or (is_union(get_origin(type_)) and any(is_none_type(tp) for tp in get_args(type_)))
 
 
-def get_type_from_optional(type: Type[Any]) -> Tuple[bool, Type[Any]]:
-    if not is_optional(type):
-        return False, type
-    return True, next(tp for tp in get_args(type) if not is_none_type(tp))
+def get_args_without_none(type_: type[Any]) -> tuple[bool, tuple[type[Any], ...]]:
+    if is_none_type(type_):
+        return True, ()
+    if is_union(get_origin(type_)):
+        args = get_args(type_)
+        is_optional_ = any(is_none_type(tp) for tp in args)
+        return is_optional_, tuple(tp for tp in args if not is_none_type(tp))
+    return False, (type_,)
 
 
-def is_sequence_type(type: Type[Any]) -> bool:
-    return check_issubclass(type, Sequence) or check_issubclass(get_origin(type), Sequence)
+def is_sequence_type(type_: type[Any]) -> TypeGuard[type[Sequence]]:
+    return check_issubclass(type_, Sequence) or check_issubclass(get_origin(type_), Sequence)  # type: ignore
 
 
-def is_sequence(value: Any) -> bool:
+def is_sequence(value: Any) -> TypeGuard[Sequence]:
     return check_isinstance(value, Sequence)
 
 
-def is_iterable_type(type: Type[Any]) -> bool:
-    return check_issubclass(type, Iterable) or check_issubclass(get_origin(type), Iterable)
+def is_iterable_type(type_: type[Any]) -> TypeGuard[type[Iterable]]:
+    return check_issubclass(type_, Iterable) or check_issubclass(get_origin(type_), Iterable)  # type: ignore
 
 
-def is_iterable(value: Any) -> bool:
+def is_iterable(value: Any) -> TypeGuard[Iterable]:
     return check_isinstance(value, Iterable)
 
 
-def is_mapping_type(type: Type[Any]) -> bool:
-    return check_issubclass(type, Mapping) or check_issubclass(get_origin(type), Mapping)
+def is_mapping_type(type_: type[Any]) -> TypeGuard[type[Mapping]]:
+    return check_issubclass(type_, Mapping) or check_issubclass(get_origin(type_), Mapping)  # type: ignore
 
 
-def is_mapping(value: Any) -> bool:
+def is_mapping(value: Any) -> TypeGuard[Mapping]:
     return check_isinstance(value, Mapping)
-
-
-def is_json_like_type(type_: Type[Any]) -> bool:
-    return check_issubclass(type_, (Mapping, Sequence)) or check_issubclass(get_origin(type_), (Mapping, Sequence))
